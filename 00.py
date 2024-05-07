b570a82a56ab66386f628b11cecf7ea2ffc6ae1d
chyijiunn <chyijiunn@gmail.com>
2024-05-07 13:48:47 +0800

    ud

---
+++ 00.py
@@ -0,0 +1,9 @@
+from machine import Pin, PWM
+from utime import sleep
+buzzer = PWM(Pin(21))
+#https://zh.wikipedia.org/zh-tw/%E9%9F%B3%E9%AB%98
+buzzer.freq(10080)
+buzzer.duty_u16(60000)
+sleep(0.5)                                                                                 
+buzzer.duty_u16(0)
+sleep(0.5)
